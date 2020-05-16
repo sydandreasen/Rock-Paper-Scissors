2738a0ca3857df09b94841b3fa85dce19b1f3a32
sydandreasen <sydandreasen@abbnebraska.com>
2020-05-16 10:42:36 -0500

    Add Stats

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 let userScore = 0;
 let opponentScore = 0;
 
+let rockTotal = 0;
+let rockWins = 0;
+let rockLosses = 0;
+let rockDraws = 0;
+
+let paperTotal = 0;
+let paperWins = 0;
+let paperLosses = 0;
+let paperDraws = 0;
+
+let scissorsTotal = 0;
+let scissorsWins = 0;
+let scissorsLosses = 0;
+let scissorsDraws = 0;
+
 const userScoreDisplay = document.getElementById("you-score");
 const opponentScoreDisplay = document.getElementById("opponent-score");
 
@@ -10,6 +25,21 @@ const message = document.querySelector(".message");
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+
+const rock_wins = document.getElementById("rock-wins");
+const rock_losses = document.getElementById("rock-losses");
+const rock_draws = document.getElementById("rock-draws");
+const rock_success = document.getElementById("rock-success");
+
+const paper_wins = document.getElementById("paper-wins");
+const paper_losses = document.getElementById("paper-losses");
+const paper_draws = document.getElementById("paper-draws");
+const paper_success = document.getElementById("paper-success");
+
+const scissor_wins = document.getElementById("scissors-wins");
+const scissor_losses = document.getElementById("scissors-losses");
+const scissor_draws = document.getElementById("scissors-draws");
+const scissor_success = document.getElementById("scissors-success");
 
 main();
 
@@ -40,16 +70,25 @@ function play(choice)
         {
             rock.classList.add('draw-border');
             undoGlow("rock", "draw-border");
+            rockTotal++;
+            rockDraws++;
+            rock_draws.innerHTML = rockDraws;
         }
         else if (choice == 2)
         {
             paper.classList.add('draw-border');
             undoGlow("paper", "draw-border");
+            paperTotal++;
+            paperDraws++;
+            paper_draws.innerHTML = paperDraws;
         }
         else if (choice == 3)
         {
             scissors.classList.add('draw-border');
             undoGlow("scissors", "draw-border");
+            scissorsTotal++;
+            scissorsDraws++;
+            scissor_draws.innerHTML = scissorsDraws;
         }
     }
     else if (choice == 1 && against == 2)
@@ -60,6 +99,9 @@ function play(choice)
         opponentScoreDisplay.innerHTML = opponentScore; 
         rock.classList.add('lose-border');
         undoGlow("rock", "lose-border");
+        rockTotal++;
+        rockLosses++;
+        rock_losses.innerHTML = rockLosses;
     }
     else if (choice == 1 && against == 3)
     {
@@ -69,6 +111,9 @@ function play(choice)
         userScoreDisplay.innerHTML = userScore;
         rock.classList.add('win-border');
         undoGlow("rock", "win-border");
+        rockTotal++;
+        rockWins++;
+        rock_wins.innerHTML = rockWins;
     }
     else if (choice == 2 && against == 1)
     {
@@ -78,6 +123,9 @@ function play(choice)
         userScoreDisplay.innerHTML = userScore;
         paper.classList.add('win-border');
         undoGlow("paper", "win-border");
+        paperTotal++;
+        paperWins++;
+        paper_wins.innerHTML = paperWins;
     }
     else if (choice == 2 && against == 3)
     {
@@ -87,6 +135,9 @@ function play(choice)
         opponentScoreDisplay.innerHTML = opponentScore;
         paper.classList.add('lose-border');
         undoGlow("paper", "lose-border");
+        paperTotal++;
+        paperLosses++;
+        paper_losses.innerHTML = paperLosses;
     }
     else if (choice == 3 && against == 1)
     {
@@ -96,6 +147,9 @@ function play(choice)
         opponentScoreDisplay.innerHTML = opponentScore;
         scissors.classList.add('lose-border');
         undoGlow("scissors", "lose-border");
+        scissorsTotal++;
+        scissorsLosses++;
+        scissor_losses.innerHTML = scissorsLosses;
     }
     else if (choice == 3 && against == 2)
     {
@@ -105,6 +159,22 @@ function play(choice)
         userScoreDisplay.innerHTML = userScore;
         scissors.classList.add('win-border');
         undoGlow("scissors", "win-border");
+        scissorsTotal++;
+        scissorsWins++;
+        scissor_wins.innerHTML = scissorsWins;
+    }
+
+    if (rockTotal > 0)
+    {
+        rock_success.innerHTML = Math.round(rockWins * 100 / rockTotal);
+    }
+    if (paperTotal > 0)
+    {
+        paper_success.innerHTML = Math.round(paperWins * 100 / paperTotal);
+    }
+    if (scissorsTotal > 0)
+    {
+        scissor_success.innerHTML = Math.round(scissorsWins * 100 / scissorsTotal);
     }
 
     function undoGlow(choice, classRemove)
